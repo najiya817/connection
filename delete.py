c367c1db9f3eda18f7dc54aabdf0bb3f0fb1586c
NAJIYA NASRIN <110896779+najiya817@users.noreply.github.com>
2022-09-12 14:55:49 +0530

    message

---
+++ delete.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+conn=mysql.connector.connect(user='root',password='abc123',host='127.0.0.1',database='mysql')
+cursor=conn.cursor()
+print("content of the table:")
+cursor.execute("SELECT*from EMPLOYEE")
+print(cursor.fetchall())
+sql="DELETE FROM EMPLOYEE WHERE AGE>'%d'" % (25)
+
+try:
+    cursor.execute(sql)
+    conn.commit()
+except:
+    conn.rollback()
+print("content of the table after delete operation")
+cursor.execute("SELECT*FROM EMPLOYEE")
+print(cursor.fetchall())
+conn.close()
